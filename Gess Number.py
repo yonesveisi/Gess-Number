586b646c8bcfc7cd6eb2bf8fa76cbb2c702ab794
yonesveisi <49885011+yonesveisi@users.noreply.github.com>
2019-11-13 22:26:34 +0330

    Create Gess Number.py

---
+++ Gess Number.py
@@ -0,0 +1,23 @@
+# programmer : younes veisi
+import random
+num = random.randint(1, 1000)
+user = int(input("Please enter your guess: "))
+Points = 20
+while 1:
+    print("Points : ", Points)
+    if user == num:
+        print("You Win")
+        Points += 1
+        break
+    elif user > num:
+        Points -= 1
+        print("please enter lower number:")
+        user = int(input("enter your guess: "))
+    elif user < num:
+        Points -= 1
+        print("please enter bigger number:")
+        user = int(input("enter your guess: "))
+    if Points == 0:
+        print("Your Point is ZERO.\n"
+              "You do not have any choice. SORRY")
+        break
